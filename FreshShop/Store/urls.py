a0cb416273d2c96980281cfcdbd70b411061634b
devil <devil@qq.com>
2019-07-22 13:46:28 +0800

    静态文件收集

--- FreshShop/Store/urls.py
+++ FreshShop/Store/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from Store import views
 
 urlpatterns = [
-    path('', )
+    # path('', )
 ]
--- FreshShop/Store/views.py
+++ FreshShop/Store/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+import hashlib
 
 # Create your views here.
+
+# 密码加密功能
+def hashpassword(password):
+    md5 = hashlib.md5()
+    md5.update(password.encode())
+    return md5.hexdigest()
